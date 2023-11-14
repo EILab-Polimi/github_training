79b34de8c3ec3250535fd670833cf33fd3b3e06e
Cadoraghese <davide.spina.95@gmail.com>
2023-11-14 16:16:33 +0100

    Revert "Add multiply"
    This reverts commit cd4f90f01a0d0f09aaff6094a0abbcbe97c33126.

--- main.py
+++ main.py
@@ -5,7 +5,3 @@ y = 6
 print(f"Let's sum {x} and {y}")
 z = sum(x, y)
 print(f"The answer is {z}")
-print()
-print(f"Let's multiply {x} and {y}")
-z = multiply(x, y)
-print(f"The answer is {z}")
--- utils.py
+++ utils.py
@@ -1,6 +1,2 @@
 def sum(a, b):
-    return a + b
-
-
-def multiply(a, b):
-    return a * b
+	return a + b
